947a504c4e28a68679c3651ac59c03cf7024b895
waleriant <tranthien.valery@gmail.com>
2025-09-24 22:46:35 +0200

    Draft of entities.py

---
+++ entities.py
@@ -0,0 +1,78 @@
+MAX_SPEED = 100
+SPEED_INCREMENT = 5
+ROTATION_INCREMENT = 5
+
+class Entity():
+    def __init__(self, x, y, speed, angle, *polygon):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.angle = angle
+        self.angle_speed = 0
+        self.polygon = list(polygon)
+
+    def __str__(self):
+        return f'''({self.x}, {self.y})
+{self.angle}°, speed={self.speed}, angle_speed={self.angle_speed}
+polygon = {self.polygon}\n'''
+
+    def rotate_right(self):
+        
+        # update angle_speed OR angle 
+
+        if self.angle_speed < 0:
+            self.angle_speed += ROTATION_INCREMENT
+            if self.angle_speed > 0:
+                self.angle_speed = 0
+        else:
+            self.angle += ROTATION_INCREMENT
+            if self.angle > 360:
+                self.angle = self.angle - 360 
+        
+        # update the polygon
+        
+        # TBD
+
+    def rotate_left(self):
+         
+        # update angle_speed OR angle 
+
+        if self.angle_speed > 0:
+            self.angle_speed -= ROTATION_INCREMENT
+            if self.angle_speed < 0:
+                self.angle_speed = 0 
+        else:
+            self.angle -= ROTATION_INCREMENT
+            if self.angle < 0:
+                self.angle = 360 + self.angle 
+
+        # update the polygon
+        
+        # TBD
+
+    def accelerate(self):
+        pass
+
+    def decelarate(self):
+        pass
+
+    def update(self):
+        self.angle += self.angle_speed
+        
+        # TBD
+
+
+if __name__ == '__main__':
+    
+    my_entity = Entity(50, 100, (0, 0), 0, (-10, 0), (10, 0), (0, 20))
+    print('Entity created.\n')
+    print(my_entity)
+
+    print('Rotate entity to the left.\n')
+    my_entity.rotate_left()
+    print(my_entity)
+
+    print('Rotate entity 2x to the right.\n')
+    my_entity.rotate_right()
+    my_entity.rotate_right()
+    print(my_entity)
